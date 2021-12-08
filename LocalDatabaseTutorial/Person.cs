1c1a4cf661fa4ff851ff4f95673e6d42dc9660f1
Дмитрий Севрюков <sevryukov.d2003@gmail.com>
2021-12-08 12:29:19 +0300

    Создал класс Person.cs и изменил код в нём

--- LocalDatabaseTutorial/Person.cs
+++ LocalDatabaseTutorial/Person.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SQLite;
 
 namespace LocalDatabaseTutorial
 {
-    class Person
+    public class Person
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
